4a268a9968f1ddd1deecbe752da09e4c1f5fddea
Kiryl Harbachonak <cool45akol@gmail.com>
2023-12-02 23:48:53 +0100

    feat: day2_1

---
+++ Day2_1.swift
@@ -0,0 +1,76 @@
+#!/usr/bin/swift
+
+import Foundation
+
+extension Sequence where Element: AdditiveArithmetic {
+  func sum() -> Element {
+    reduce(into: .zero, +=)
+  }
+}
+
+extension String { 
+	var isNotEmpty: Bool { !isEmpty }
+}
+
+func day2_1(input: String) -> UInt {
+	enum CubeColor: String, Hashable { 
+		case red, green, blue
+	}
+
+	var posibleGameIDs = [UInt]()
+	let maxReds: UInt = 12
+	let maxGreens: UInt = 13
+	let maxBlues: UInt = 14
+
+	let regex = #/(?:\bGame (?<id>\d+)\b:)?((?<game>.+?)(?:;|$))/#
+	for line in input.components(separatedBy: .newlines) { 
+		let result = try! regex.wholeMatch(in: line)
+		
+		let gameID = UInt(result!.id!)!
+		var isGamePosible = true
+
+		let rawSets = result!.game.components(separatedBy: ";")
+		for rawSet in rawSets { 
+			let rawCubes = rawSet.components(separatedBy: ",")
+			
+			for rawCube in rawCubes {
+				let rawCubeComponents = rawCube.components(separatedBy: " ")
+					.filter(\.isNotEmpty)
+				let cubeCount = UInt(rawCubeComponents.first!)!
+				let cubeColor = CubeColor(rawValue: rawCubeComponents.last!)!
+
+				switch cubeColor { 
+				case .red:
+					if cubeCount > maxReds {
+						isGamePosible = false
+						break
+					} 
+				case .green:
+					if cubeCount > maxGreens {
+						isGamePosible = false
+						break
+					} 
+				case .blue:
+					if cubeCount > maxBlues {
+						isGamePosible = false
+						break
+					} 
+				}
+			}
+		}
+		guard isGamePosible else { continue }
+		posibleGameIDs.append(gameID)
+	}
+
+	return posibleGameIDs.sum()
+}
+
+do {
+	let text = try String(
+		contentsOf: URL(fileURLWithPath: "Inputs/day2_1.txt"), 
+		encoding: .utf8
+	)
+	print(day2_1(input: text))
+} catch {
+	print("Error reading file: \(error)")
+}
--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ day1_1:
 
 day1_2:
 	@./Day1_2.swift
+
+day2_1:
+	@./Day2_1.swift
+
